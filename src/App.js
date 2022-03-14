6590a72fbfd563875717c4823d9e1fc9f1f6712a
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2022-03-15 01:45:15 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.scss";
 import { getMedicineData } from "./redux/actions/medicines";
 import { useDispatch } from "react-redux";
-
 import Home from "./screens/Home";
-
 import "./styles/Home/styles.scss";
+
+// Used redux so that in the future the crud part will be handled easily
 
 function App() {
   const dispatch = useDispatch();
